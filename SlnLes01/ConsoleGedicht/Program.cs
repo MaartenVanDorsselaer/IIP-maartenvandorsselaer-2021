e1a922a0bf48bb44bb85b219abba8fdaca6e6725
Maarten Van Dorsselaer <maarten.vandorsselaer@student.odisee.be>
2020-10-19 16:48:08 +0200

    Console oefeningen hoofdstuk 2

---
+++ SlnLes01/ConsoleGedicht/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGedicht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jantje zag eens pruimen hangen,");
+            Console.ReadKey();
+            Console.WriteLine("O! Als eieren zo groot.");
+            Console.ReadKey();
+            Console.WriteLine("'t Scheen, dat Jantje wou gaan plukken,");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SlnLes01/ConsoleHello/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Hoe heet je? ");
+            string name = Console.ReadLine();
+            Console.Write("Aangename kennismaking, ");
+            Console.WriteLine(name);
+            Console.WriteLine("druk op een toets om verder te gaan...");
+            Console.ReadKey();
+        }
+    }
+}
