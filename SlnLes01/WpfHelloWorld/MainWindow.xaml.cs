6037497355126df413cb3b90bdc83946327d8b8f
Maarten Van Dorsselaer <maarten.vandorsselaer@student.odisee.be>
2020-10-19 14:26:28 +0200

    Toevoegen Project

--- SlnLes01/WpfHelloWorld/MainWindow.xaml.cs
+++ SlnLes01/WpfHelloWorld/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfHelloWorld
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lblMyLabel.Content = "Hallo, " + txtNaam.Text;
+        }
     }
 }
