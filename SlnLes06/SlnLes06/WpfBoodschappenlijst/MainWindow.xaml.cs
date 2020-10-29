ca686496fdade728b5d1a0e4c93b580962523415
Maarten Van Dorsselaer <maarten.vandorsselaer@student.odisee.be>
2020-10-29 19:12:23 +0100

    wpfBoodschappenlijst werkt

--- SlnLes06/SlnLes06/WpfBoodschappenlijst/MainWindow.xaml.cs
+++ SlnLes06/SlnLes06/WpfBoodschappenlijst/MainWindow.xaml.cs
@@ -21,43 +21,30 @@ namespace WpfBoodschappenlijst
     public partial class MainWindow : Window
     {
         string boodschappenlijst;
-        string boodschap;
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        //private void ListBoxItems_Selected(object sender, RoutedEventArgs e)
-        //{
-        //    // geeft telkens een foutmelding dat een object van type selectedItemCollection niet geconverteerd kan worden naar listBoxItem
-        //    //ListBoxItem selectedItem = (ListBoxItem)lbxBoodschappen.SelectedItems;
-        //    //foreach (object boodschap in lbxBoodschappen.SelectedItems)
-        //    //{
-        //    //    boodschappenlijst += selectedItem.Content;
-        //    //}
-        //    //lblBoodschappenlijst.Content = $"Je selecteerde: {boodschappenlijst}";
-            
-        //}
-
         private void lbxBoodschappen_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // zou selectedItems moeten zijn, maar geeft telkens een exception not handled error, op deze manier werkt de app wel (half)
-            // ListBoxItem selectedItem = (ListBoxItem)lbxBoodschappen.SelectedItem;
 
             // for loop om elk item te doorlopen en toe te voegen aan de boodschappenlijst
             for (int i = 0; i < lbxBoodschappen.Items.Count; i++)
             {
+                // gaat elk item af
                 ListBoxItem selectedItem = (ListBoxItem)lbxBoodschappen.Items.GetItemAt(i);
                 if (selectedItem.IsSelected == true)
                 {
                     // doorloopt elk listboxitem en onthoudt het laatste argument dat enabled is, dus als je eerst appel aanklikt en dan iets anders, toont het enkel appel
-                    boodschap = " " + Convert.ToString(selectedItem.Content);
+                    boodschappenlijst += " " + Convert.ToString(selectedItem.Content);
                 }
             }
             // ik liet eerst de boodschappenlijst in de if maken, maar dit herhaalde dan telkens een aantal boodschappen
-            // op deze manier geeft hij de juiste lijst weer, maar ik vind het niet logisch
-            boodschappenlijst += boodschap;
+            // op deze manier geeft hij wel de lijst weer
             lblBoodschappenlijst.Content = $"Je selecteerde: {boodschappenlijst}";
+            // boodschap leegmaken zodat er geen herhaling komt van de nieuwe boodschappenlijst, maar dat de selecties die niet meer geselectteerd zijn
+            // ook niet meer in de boodschappenlijst zitten
+            boodschappenlijst = "";
         }
     }
 }
