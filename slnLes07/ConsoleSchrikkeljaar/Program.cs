5946c2ec9b937bf3d4b3ab8d146d986b2e3c4b5b
Maarten Van Dorsselaer <maarten.vandorsselaer@student.odisee.be>
2020-11-09 11:29:00 +0100

    alle console apps h7 methoden klaar

---
+++ slnLes07/ConsoleSchrikkeljaar/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSchrikkeljaar
+{
+    class Program
+    {
+        static private string Schrikkeljaar(int jaartal)
+        {
+            // variabele om antwoord in op te slaan
+            string antwoord="";
+            // controleren of het een schrikkeljaar is of niet
+            if (jaartal % 4 == 0)
+            {
+                if (jaartal % 100 == 0)
+                {
+                    if (jaartal % 400 == 0)
+                    {
+                        antwoord += "een";
+                    }
+                    else
+                    {
+                        antwoord += "géén";
+                    }
+                }
+                else
+                {
+                    antwoord += "een";
+                }
+            }
+            // antwoord naar main sturen
+            return antwoord;
+        }
+        static void Main(string[] args)
+        {
+            // variabele
+            int jaartal;
+            // header
+            Console.WriteLine("SCHRIKKELJAAR");
+            Console.WriteLine("=============");
+            do
+            {
+                // jaartal inlezen, in een variabele steken, doorsturen naar methode en met antwoord van methode de output bepalen
+                Console.Write("Geef een jaartal: ");
+                jaartal = Convert.ToInt32(Console.ReadLine());
+                string antwoord = Schrikkeljaar(jaartal);
+                Console.WriteLine($"Het jaartal {jaartal} is {antwoord} schrikkeljaar");
+
+            }
+            // stopconditie
+            while (jaartal >0);
+
+        }
+    }
+}
---
+++ slnLes07/ConsoleTemperatuur/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTemperatuur
+{
+    class Program
+    {
+        // celsius omzetten naar fahrenheit
+        private static double CelsiusToFahrenheit(double graden)
+        {
+            double omzetting = graden*1.8 + 32;
+            return omzetting;
+        }
+        // fahrenheit omzetten naar celsius
+        private static double FahrenheitToCelsius(double graden)
+        {
+            double omzetting = (graden-32) / 1.8;
+            return omzetting;
+        }
+        // celsius omzetten naar kelvin
+        private static double CelsiusToKelvin(double graden)
+        {
+            double omzetting = graden+273.15;
+            return omzetting;
+        }
+        // kelvin omzetten naar celsius
+        private static double KelvinToCelsius(double graden)
+        {
+            double omzetting = graden-273.15;
+            return omzetting;
+        }
+        static void Main(string[] args)
+        {
+            // variabelen aanmaken
+            double omzetting;
+            double graden;
+            string keuze;
+            // banner
+            Console.WriteLine("TEMPERATUURCONVERSIE");
+            Console.WriteLine("====================");
+            
+            do
+            {
+                // menu weergeven
+                Console.WriteLine(@"a. Celsius naar Fahrenheit
+b. Fahrenheit naar Celsius
+c. Celsius naar Kelvin
+d. Kelvin naar Celsius
+x. Afsluiten" + Environment.NewLine);
+                Console.Write("Maak je keuze: ");
+                keuze = Console.ReadLine();
+                // juiste methode aanspreken per keuze + antwoord weergeven
+                if (keuze == "a")
+                {
+                    Console.Write("Celsius: ");
+                    graden = Convert.ToDouble(Console.ReadLine());
+                    omzetting = CelsiusToFahrenheit(graden);
+                    Console.WriteLine($"Fahrenheit: {omzetting}");
+                }
+                else if (keuze == "b")
+                {
+                    Console.Write("Fahrenheit: ");
+                    graden = Convert.ToDouble(Console.ReadLine());
+                    omzetting = FahrenheitToCelsius(graden);
+                    Console.WriteLine($"Celsius: {omzetting}");
+                }
+                else if (keuze == "c")
+                {
+                    Console.Write("Celsius: ");
+                    graden = Convert.ToDouble(Console.ReadLine());
+                    omzetting = CelsiusToKelvin(graden);
+                    Console.WriteLine($"Kelvin: {omzetting}");
+                }
+                else if (keuze == "d")
+                {
+                    Console.Write("Kelvin: ");
+                    graden = Convert.ToDouble(Console.ReadLine());
+                    omzetting = KelvinToCelsius(graden);
+                    Console.WriteLine($"Celsius: {omzetting}");
+                }
+                Console.ReadLine();
+                Console.Clear();
+            } while (keuze != "x");
+        }
+    }
+}
